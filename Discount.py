8f3a3b9b6bbce178603f2e88612d462e39b0b5f6
Henry Dom Ali <114635034+DvineConqueror@users.noreply.github.com>
2023-09-16 00:53:27 +0800

    Added else for "no discount"

--- Discount.py
+++ Discount.py
@@ -4,17 +4,19 @@ quantity = float(input("Enter Quantity: "))
 total = price * quantity
 print(f"Total Payable: {total}")
 
-if total >= 1000:
-    discount = total / 10
-    new_total = total - discount
-    print("10% discount available!")
-    print(f"Amount Payable: {new_total}")
-    
-elif total >= 500:
+
+if total >= 500:
     discount = total / (100 / 5)
     new_total = total - discount
     print("5% discount available!")
     print(f"Amount Payable: {new_total}")
     
+elif total >= 1000:
+    discount = total / 10
+    new_total = total - discount
+    print("10% discount available!")
+    print(f"Amount Payable: {new_total}")
+    
 else:
-    print("Invalid Input")
+    print("No discount applied")
+    print(f"Amount Payable: {total}")
