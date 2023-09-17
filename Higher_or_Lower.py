53f214c69c4025415b23934e41d13bacc9183946
Henry Dom Ali <114635034+DvineConqueror@users.noreply.github.com>
2023-09-17 17:59:00 +0800

    Added High Score to the game

--- Higher_or_Lower.py
+++ Higher_or_Lower.py
@@ -1,32 +1,59 @@
 import random
 
-print("============MENU===========")
-print("Welcome to Higher or Lower Game!")
-print("Rules: \n You will enter the range of the number\n that you are going to guess. \n Guess the number, and if your guess \n is higher or lower\n It will determine it.")
-
-while True:
-    number_range = int(input("Enter number's range: "))
-    number = random.randint(1, number_range)
-    user_input = 0
-    guesses = 5
+def menu():
+    print("============MENU===========")
+    print("Welcome to Higher or Lower Game!")
+    print("Rules: \n You will enter the range of the number\n that you are going to guess. \n Guess the number, and if your guess \n is higher or lower\n It will determine it.")
+    print("============MENU===========")
+    player_register()
     
-    while user_input != number and guesses > 0:
-        user_input = int(input("Guess the number: "))
-        if user_input > number:
-            guesses = guesses - 1
-            print("Lower")
-            print(f"Guesses left: {guesses}")
-        elif user_input < number:
-            guesses = guesses - 1
-            print("Higher")
-            print(f"Guesses left: {guesses}")
-        elif user_input == number:
-            print("You WON!")
-            print(f"Correct! The number is: {number} and your guesses left is: {guesses}")
-            
-    choice = input("Do you want to continue(Yes or No): ")
-    if choice.lower() != "yes":
-        print("Thank you for playing!")
-        break
+def player_register():
+    start = input("Do you want to start the game (Yes or No): ")
+    if start.lower() != 'yes': 
+        print("Ending the game!")
     else:
-        continue
+        register = input("Enter your name as Player: ")
+        print(f"Player Name: {register}")
+        main_game()
+
+def main_game():
+    high_score = 0
+    while True:
+        number_range = int(input("Enter number's range: "))
+        number = random.randint(1, number_range)
+        user_input = 0
+        max_guesses = 5
+        guesses = max_guesses  # Initialize guesses to max_guesses for each round
+
+        while user_input != number and guesses > 0:
+            user_input = int(input("Guess the number: "))
+            if user_input > number:
+                guesses -= 1  # Use the shorthand to decrement guesses
+                print("Lower")
+                print(f"Guesses left: {guesses}")
+            elif user_input < number:
+                guesses -= 1  # Use the shorthand to decrement guesses
+                print("Higher")
+                print(f"Guesses left: {guesses}")
+            elif user_input == number:
+                points = (max_guesses - guesses) + 1
+                points = points * 100
+                print("You WON!")
+                print(f"Correct! The number is: {number} and your guesses left is: {guesses}")
+                print(f"You scored {points} points!")
+
+                if points > high_score:
+                    high_score = points
+
+                print(f"Your High Score is: {high_score}")
+                
+        choice = input("Do you want to continue(Yes or No): ")
+        if choice.lower() != "yes":
+            print("Thank you for playing!")
+            break
+        else:
+            continue
+        
+
+        
+menu()
