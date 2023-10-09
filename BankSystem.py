c6cb2756576e14111d121be2900a29eb7d9c4ce1
Henry Dom Ali <114635034+DvineConqueror@users.noreply.github.com>
2023-10-09 22:21:40 +0800

    Fixed the bugs
    Fixed the continuously looping when bank system is exited.

--- BankSystem.py
+++ BankSystem.py
@@ -15,19 +15,22 @@ def create_account():
     login_account()
     
 def login_account():
-    while login_account != False:
+    logged_in = False
+    while not logged_in:
         os.system("cls")
         print("DOMINIC BANKING SYSTEM")
         print("Login Account")
         userName_true = input("Enter your username: ")
         password_true = input("Enter your password: ")
         print("======================")
-        
         if userName != userName_true or password != password_true:
             print("Invalid username. Try again!")
-            time.sleep(2)
+            time.sleep(1.5)
         else:
+            logged_in = True
             bank_system()
+            
+            
       
 account_balance = 0 #Defined Account Balance outside
   
@@ -35,44 +38,45 @@ def bank_system():
     
     global account_balance #declare account balance to global to continuously update when the program checks balance
     
-    print("DOMINIC BANKING SYSTEM")
-    print("Welcome!", userName)
-    print("Menu")
-    print("Balance:", account_balance)
-    print("[1]Deposit [2]Withdraw [3]Exit")
-    choice = int(input("Enter your choice: "))
-    
-    match choice:
-        case 1:
-            deposit = int(input("Enter how much you want to deposit: "))
-            if deposit > 0:
-                account_balance += deposit
-                print("You have successfully deposited:", deposit)
-                time.sleep(2)
-                os.system("cls")
-                bank_system()
-            else:
-                print("Invalid amount to deposit")
-        case 2:
-            withdraw = int(input("Enter how much you want to withdraw: "))
-            if withdraw <= account_balance:
-                account_balance -= withdraw
-                print("Withdrawn amount is: ", withdraw)
-                time.sleep(2)
-                os.system("cls")
-                bank_system()
-            else:
-                print("Invalid amount to withdraw")
-                retry = input("Would you like to continue? yes/no: ")
-                if retry.lower() != 'yes':
-                    print("Have a nice day!")
-                else:
+    while True:
+        print("DOMINIC BANKING SYSTEM")
+        print("Welcome!", userName)
+        print("Menu")
+        print("Balance:", account_balance)
+        print("[1]Deposit [2]Withdraw [3]Exit")
+        choice = int(input("Enter your choice: "))
+        match choice:
+            case 1:
+                deposit = int(input("Enter how much you want to deposit: "))
+                if deposit > 0:
+                    account_balance += deposit
+                    print("You have successfully deposited:", deposit)
+                    time.sleep(2)
                     os.system("cls")
-                    bank_system()
-        case 3:
-            print("Bank System exiting...")
-            time.sleep(2)
-            os.system("cls")         
+                else:
+                    print("Invalid amount to deposit")
+                    os.system("cls")
+            case 2:
+                withdraw = int(input("Enter how much you want to withdraw: "))
+                if withdraw <= account_balance:
+                    account_balance -= withdraw
+                    print("Withdrawn amount is: ", withdraw)
+                    time.sleep(2)
+                    os.system("cls")
+                else:
+                    print("Insufficient Funds!")
+                    retry = input("Would you like to continue? yes/no: ")
+                    if retry.lower() != 'yes':
+                        print("Have a nice day!")
+                        time.sleep(1.5)
+                        print("Bank System exiting...")
+                        break
+                    else:
+                        os.system("cls")
+                        continue
+            case 3:
+                print("Bank System exiting...")
+                return True   
     
 def login():
     print("DOMINIC BANKING SYSTEM")
@@ -83,6 +87,8 @@ def login():
     else:
         print("Invalid")
     print("======================")
+    
+
         
 login()
         
