48e2257126f957f751a6daf45aaf69c9bb5c4aa1
Henry Dom Ali <114635034+DvineConqueror@users.noreply.github.com>
2023-10-12 22:52:16 +0800

    Created a .exe of py
    Created .exe of python file, new bug is found: When withdrawing a negative amount of number, it registers, and the negative withdrawn amount increments to be the new account balance. NEED TO FIX.

--- Final-Project/BankSystem.py
+++ Final-Project/BankSystem.py
@@ -6,7 +6,7 @@ def login():
     while True:
         os.system("cls")
         print("********** Doynamic Finance **********")
-        print("Would you like create an account?")
+        print("Would you like to create an account?")
         print("1. Yes")
         print("2. No")
         choice = input("Enter your choice: ")#these are in string, they should accept a string instead of int
@@ -86,20 +86,21 @@ def bank_system():
     while True:
         print("********** Doynamic Finance **********")
         print(f"---------- Welcome! {userName} ----------")
-        print(f"============== Balance: {account_balance} =============")
+        print(f"=========== Balance: {account_balance} ==========")
         print("+++|[1]Deposit [2]Withdraw [3]Exit|+++")
-        choice = int(input("Enter your choice: "))
+        choice = input("Enter your choice: ")
         
         password_attempt = pwinput.pwinput(prompt='Enter your password to continue: ', mask='*')
     
         match choice:
-            case 1:
+            case "1":
                 deposit = int(input("Enter how much you want to deposit: "))
                 if deposit > 0:
                     if password_attempt == password:
                         account_balance += deposit
                         time.sleep(1.5)
                         print("You have successfully deposited:", deposit)
+                        print("Going back to Menu...")
                         time.sleep(1.5)
                         os.system("cls")
                     else:
@@ -113,13 +114,14 @@ def bank_system():
                     print("Invalid amount to deposit")
                     time.sleep(1.2)
                     os.system("cls")
-            case 2:
+            case "2":
                 withdraw = int(input("Enter how much you want to withdraw: "))
                 if withdraw <= account_balance:
                     if password_attempt == password:
                         account_balance -= withdraw
-                        time.sleep()
+                        time.sleep(1.5)
                         print("Withdrawn amount is: ", withdraw)
+                        print("Going back to Menu...")
                         time.sleep(1.5)
                         os.system("cls")
                     else:
@@ -133,20 +135,24 @@ def bank_system():
                     print("Insufficient Funds!")
                     retry = input("Would you like to continue? yes/no: ")
                     if retry.lower() != 'yes':
-                        print("Have a nice day!")
+                        print("Program exiting...")
                         time.sleep(1.5)
-                        print("Bank System exiting...")
+                        print("Have a nice day!")
+                        print("********** Doynamic Finance **********")
                         break
                     else:
                         os.system("cls")
                         continue
-            case 3:
-                print("Bank System exiting...")
-                print("********** Doynamic Finance **********")
-                time.sleep(0.9)
-                return True   
-    
-    
+            case "3":
+                if password_attempt == password:
+                    print("Bank System exiting...")
+                    print("********** Doynamic Finance **********")
+                    time.sleep(0.9)
+                    return True
+                else:
+                        print("Entered password is wrong.")
+                        time.sleep(1.5)
+                        os.system("cls")
         
 login()
         
