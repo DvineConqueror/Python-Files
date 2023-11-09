10ad60561c4edc2ca2facdd717e98c5251df0198
Henry Dom Ali <114635034+DvineConqueror@users.noreply.github.com>
2023-11-10 01:42:54 +0800

    Update BankSystem_GUI.py
    Fixed window frames and widgets.

--- Final-Project/BankSystem_GUI.py
+++ Final-Project/BankSystem_GUI.py
@@ -1,6 +1,6 @@
 import customtkinter
 import os
-from PIL import Image
+from PIL import ImageTk
 from tkinter import messagebox
 from tkinter import Toplevel
 
@@ -11,6 +11,10 @@ def main_menu():
     main_menu_window = customtkinter.CTk()
     main_menu_window.geometry("600x400")
     main_menu_window.mainloop()
+    
+def exit_system():
+    messagebox.showinfo("Doynamic Banking", "Thank you for using Doynamic Bank!")
+    exit()
 
 def validate_password(password):
     if len(password) < 8:
@@ -24,22 +28,30 @@ def validate_password(password):
     return True
 
 def create_account_section():
-    create_window = Toplevel(root)
-    create_window.geometry("400x260")
+    create_window = Toplevel(root, background="black")
+    create_window.geometry("400x282")
+    create_window.resizable(0,0)
     create_window.title("Create Account")
     root.withdraw()
 
     frame_create = customtkinter.CTkFrame(master=create_window)
-    frame_create.pack(pady=30, padx=30, fill="both", expand=False)
+    frame_create.pack_propagate(False)
+    frame_create.pack(pady=0, padx=0, fill="both", expand=True)
 
-    label_create = customtkinter.CTkLabel(master=frame_create, text="Create an account", font=("Times", 20))
-    label_create.pack(pady=12, padx=10)
+    label_create = customtkinter.CTkLabel(master=frame_create, text="Create an account", font=("Times", 32), text_color="#1DDEA8")
+    label_create.pack(pady=(12, 0), padx=10)
+    
+    note = customtkinter.CTkLabel(master=frame_create, text="Password should contain:", text_color="#DA5D74",font=("Times", 20))
+    note.pack(pady=(0, 0))
+    
+    label_remind = customtkinter.CTkLabel(master=frame_create, text="\nMinimum of 8 Characters, One Uppercase Letter\nOne Numeric Value",font=("Times", 14))
+    label_remind.pack(pady=(0, 0))
 
-    username_create = customtkinter.CTkEntry(master=frame_create, placeholder_text="Username", font=("Times", 14))
-    username_create.pack(pady=12, padx=10)
+    username_create = customtkinter.CTkEntry(master=frame_create, placeholder_text="Username", font=("Times", 18), width=200)
+    username_create.pack(pady=(12, 0), padx=10)
 
-    password_create = customtkinter.CTkEntry(master=frame_create, placeholder_text="Password", show="*", font=("Times", 14))
-    password_create.pack(pady=12, padx=10)
+    password_create = customtkinter.CTkEntry(master=frame_create, placeholder_text="Password", show="*", font=("Times", 18), width=200)
+    password_create.pack(pady=(22, 0), padx=10)
 
     def create_account():
         global created_username
@@ -54,30 +66,32 @@ def create_account_section():
         else:
             messagebox.showerror("Invalid Password", "Password does not meet the criteria.")
 
-    create_button = customtkinter.CTkButton(master=frame_create, border_color="red", text="Create Account", command=create_account, font=("Times", 14))
-    create_button.pack(pady=12, padx=10)
+    create_button = customtkinter.CTkButton(master=frame_create, border_color="red", text="Create Account", command=create_account, font=("Times", 20))
+    create_button.pack(pady=(14, 0), padx=10)
     create_window.bind("<Return>", lambda event=None: create_account())
 
+attempts = 0  # Move the attempts variable outside the function
+
 def login_account_section():
-    customtkinter.set_appearance_mode("dark")
-    customtkinter.set_default_color_theme("green")
     login_window = Toplevel(root)
-    login_window.geometry("400x260")
+    login_window.geometry("400x282")
+    login_window.resizable(0, 0)
     login_window.title("Login")
 
     frame_login = customtkinter.CTkFrame(master=login_window)
-    frame_login.pack(pady=30, padx=30, fill="both", expand=False)
+    frame_login.pack(pady=0, padx=0, fill="both", expand=True)
 
-    label_login = customtkinter.CTkLabel(master=frame_login, text="Login", font=("Times", 20))
+    label_login = customtkinter.CTkLabel(master=frame_login, text="Login", font=("Times", 32))
     label_login.pack(pady=12, padx=10)
 
-    username_login = customtkinter.CTkEntry(master=frame_login, placeholder_text="Username", font=("Times", 14))
+    username_login = customtkinter.CTkEntry(master=frame_login, placeholder_text="Username", font=("Times", 18), width=200)
     username_login.pack(pady=12, padx=10)
 
-    password_login = customtkinter.CTkEntry(master=frame_login, placeholder_text="Password", show="*", font=("Times", 14))
+    password_login = customtkinter.CTkEntry(master=frame_login, placeholder_text="Password", show="*", font=("Times", 18), width=205)
     password_login.pack(pady=12, padx=10)
 
     def login_attempt():
+        global attempts
         entered_username = username_login.get()
         entered_password = password_login.get()
 
@@ -86,9 +100,13 @@ def login_account_section():
             login_window.destroy()  # Close the login window
             main_menu()
         else:
+            attempts += 1
+            if attempts > 3:
+                messagebox.showerror("Login failed", "Too many failed attempts!")
+                exit_system()
             messagebox.showerror("Login Failed", "Please check your credentials.")
 
-    login_button = customtkinter.CTkButton(master=frame_login, border_color="red", text="Login", command=login_attempt, font=("Times", 14))
+    login_button = customtkinter.CTkButton(master=frame_login, border_color="#000000", text="Login", command=login_attempt, font=("Times", 14), width=200)
     login_button.pack(pady=12, padx=10)
 
     # Bind the Enter key to the login button
@@ -96,10 +114,20 @@ def login_account_section():
 
 root = customtkinter.CTk()
 root.geometry("400x260")
+root.resizable(0,0)
 root.title("Welcome to Doynamic Bank!")
+root.iconpath = ImageTk.PhotoImage(file=os.path.join("Final-Project","BankImage.png"))
+root.wm_iconbitmap()
+root.iconphoto(False, root.iconpath)
+
+left_frame = customtkinter.CTkFrame(master=root)
+left_frame.pack(padx=10, pady=0, side="left", fill="y", expand=True)
+
+root.columnconfigure(0, weight=90)
+root.columnconfigure(1, weight=10)
 
 frame = customtkinter.CTkFrame(master=root)
-frame.pack(pady=30, padx=30, expand=True)
+frame.pack(pady=0, padx=0,side="left", fill="both", expand=True)
 
 label = customtkinter.CTkLabel(master=frame, text="Doynamic Bank", font=("Times", 42))
 label.pack(pady=12, padx=30)
@@ -108,8 +136,10 @@ label.pack(pady=12, padx=30)
 label = customtkinter.CTkLabel(master=frame, text="Would you like to", font=("Times", 20))
 label.pack(pady=5, padx=30)
 
-# Create Account Section
-create_button = customtkinter.CTkButton(master=frame, border_color="red", text="Create Account", command=lambda:create_account_section(), font=("Times", 20))
+create_button = customtkinter.CTkButton(master=frame, fg_color="#21866A", text="Create Account", command=lambda:create_account_section(), font=("Times", 20), width=180, height=35)
 create_button.pack(pady=12, padx=10)
+
+exit_button = customtkinter.CTkButton(master=frame, border_color="red", text="Exit", command=lambda:exit_system(), font=("Times", 20), width=180, height=35)
+exit_button.pack(pady=12, padx=10)
 
 root.mainloop()
